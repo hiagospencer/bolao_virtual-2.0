bc67c11029d7d364e703186f7873fcaa5332a5b6
hiagospencer <hiaguinhospencer@gmail.com>
2025-06-09 20:44:12 -0300

    Configurando o envio de email quando redefinir a senha

--- bolao_virtual/settings.py
+++ bolao_virtual/settings.py
@@ -160,6 +160,14 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT"))  # convertendo para inteiro
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == 'True'  # convertendo para booleano
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
 
